1aaefb5862fd791e1bdccb6b4d6f4af7347e1c05
Shyam792004 <124516994+Shyam792004@users.noreply.github.com>
2024-10-07 21:57:02 +0530

    Add files via upload

---
+++ backend/Genz-Code/src/main/java/com/kenny/genzvehicle/auth/RegisterRequest.java
@@ -0,0 +1,19 @@
+package com.kenny.genzvehicle.auth;
+
+import com.kenny.genzvehicle.enums.Role;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class RegisterRequest {
+
+    private String name;
+    private String email;
+    private String password;
+    private String phoneNo;
+}
